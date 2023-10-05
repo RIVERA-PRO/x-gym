717b7a29699ecb4b8a9b8c2a6fcab1343f54c1ea
Juan Rivera <indiojuan2012.jr@gmail.com>
2023-10-05 19:40:50 -0300

    rutes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+ redirectTo:'/home',
+ pathMatch:'full'
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent, 
-    NavbarComponent, HomeComponent
+    NavbarComponent, 
+    HomeComponent, 
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes), // Aquí pasas el arreglo de rutas como argumento
   ],
   providers: [],
   bootstrap: [AppComponent] 
